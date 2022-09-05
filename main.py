5c66f108de57e55ae17663865311ef3063e66724
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2022-09-05 00:07:08 +0000

    i edited the function and checked if my lists worked

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-def book_store (title, author, year_of_publishing, genre, pages):
+def book_store ():
+ 
+ title = input("Enter books title: ")
+ bookTitles.append(title)
+ author = input ("Enter books author: ")
+ bookAuthors.append(author)
+ year_of_publishing = input ("Enter books year of publishing: ")
+ bookYear.append(year_of_publishing)
+ genre = input ("what genre is {}: ".format(title))
+ bookGenre.append(genre)
+ pages = input ("how many pages does {} have?: ".format(title))
+ bookPages.append(pages)
     
-    # title = input("Enter books title: ")
-    # author = input ("Enter books author: ")
-    # year_of_publishing = input ("Enter books year of publishing: ")
-    # genre = input ("what genre is {}: ".format(title))
-    # pages = input ("how many pages does {} have?: ".format(title))
-
-    print ("Title: " + title + ", Author: " + author + ", Published: " + 
-  year_of_publishing + ", Genre: " + genre + ", Pages: " + pages)
 
 bookTitles = [] 
 bookAuthors = []
@@ -15,16 +18,10 @@ bookYear = []
 bookGenre = []
 bookPages = []
 
-title = input("Enter books title: ")
-bookTitles.append(title)
-author = input ("Enter books author: ")
-bookAuthors.append(author)
-year_of_publishing = input ("Enter books year of publishing: ")
-bookYear.append(year_of_publishing)
-genre = input ("what genre is {}: ".format(title))
-bookGenre.append(genre)
-pages = input ("how many pages does {} have?: ".format(title))
-bookPages.append(pages)
 
-book_store(title, author, year_of_publishing, genre, pages)
+book_store()
+book_store()
+
+
+print ("Title: {}, Author: {}, Published: {}, Genre: {}, Pages: {}," .format(bookTitles, bookAuthors, bookYear, bookGenre, bookPages ))
 print(bookTitles, bookAuthors, bookYear, bookGenre, bookPages)
