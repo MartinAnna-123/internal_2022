746d7790393d565679497e8dfb795f7807fc1592
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2022-09-06 09:16:14 +0000

    added in a book_info function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def book_store ():
+def book_list ():
  
  title = input("Enter books title: ")
  bookTitles.append(title)
@@ -10,18 +10,50 @@ def book_store ():
  bookGenre.append(genre)
  pages = input ("how many pages does {} have?: ".format(title))
  bookPages.append(pages)
-    
 
-bookTitles = [] 
-bookAuthors = []
-bookYear = []
-bookGenre = []
-bookPages = []
+def book_info ():
+  a = bookTitles.index(book)
+  choice = input("what do you want to know about {}. Please type: Author, Year,  Genre  or Pages".format(book))
+  if (choice == "Author") or (choice == "author"):
+    print("the author of" + book + "is {}".format(bookAuthors(int(a))))
+  elif (choice == "Year") or (choice == "year"):
+    print("the year " + book + " was published was {} ".format(bookYear(int(a))))
+  elif (choice == "Genre") or (choice == "genre"):
+    print("the genre of " + book +  " is {}".format(bookGenre(int(a))))
+  elif (choice == "Pages") or (choice == "pages"):
+    print(book  + "has {} pages".format(bookPages(int(a))))
+  else:
+    print("you didn't type anything correctly")
+    book_info()
 
+bookTitles = ["Hunger Games", "Little Women"] 
+bookAuthors = ["Suzanne Collins", "Lousia May Alcott"]
+bookYear = ["2008","1868"]
+bookGenre = ["dystopian", "coming-of-age"]
+bookPages = ["600", "670"]
 
-book_store()
-book_store()
+print("lets add some more books to the list")
+book_list()
+
 
 
 print ("Title: {}, Author: {}, Published: {}, Genre: {}, Pages: {}," .format(bookTitles, bookAuthors, bookYear, bookGenre, bookPages ))
-print(bookTitles, bookAuthors, bookYear, bookGenre, bookPages)
+
+repeat = True
+while repeat == True:
+  question = input("do you want to add a book to the list?")
+  if question == "yes":
+    book_list()
+  else:
+    question2 = input("do you want to find out information about a book?")
+    if question2  == "yes":
+      book = input("what book do  you want to know information about?")
+      book_info()
+    else:
+      "ok"
+      repeat = False
+    
+
+print ("Title: {}, Author: {}, Published: {}, Genre: {}, Pages: {}," .format(bookTitles, bookAuthors, bookYear, bookGenre, bookPages ))
+
+
