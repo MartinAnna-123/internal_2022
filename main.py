e9f7cbfcbc77c2d48ea715882d3e7bf32684c5a2
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2022-09-07 02:04:39 +0000

    trying to add a dictionary and it is NOT GOING WELLL

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 def book_list ():
  
  title = input("Enter books title: ")
- bookTitles.append(title)
+ bookdict.update({"title" : str(title)})
  author = input ("Enter books author: ")
- bookAuthors.append(author)
- year_of_publishing = input ("Enter books year of publishing: ")
- bookYear.append(year_of_publishing)
+ bookdict.update({"author" : str(author)})
+ year = input ("Enter books year of publishing: ")
+ bookdict.update({"year" : str(year)})
  genre = input ("what genre is {}: ".format(title))
- bookGenre.append(genre)
+ bookdict.update({"genre" : str(genre)})
  pages = input ("how many pages does {} have?: ".format(title))
- bookPages.append(pages)
+ bookdict.update({"pages" : str(pages)})
 
 def book_info ():
-  a = bookTitles.index(book)
-  choice = input("what do you want to know about {}. Please type: Author, Year,  Genre  or Pages".format(book))
+  a = bookdict.get(book)
+  choice = input("what do you want to know about {}. Please type: Author, Year,  Genre  or Pages: ".format(book))
   if (choice == "Author") or (choice == "author"):
-    print("the author of" + book + "is {}".format(bookAuthors(int(a))))
+    print("the author of" + book + "is {}".format(bookdict.get(int(a))))
   elif (choice == "Year") or (choice == "year"):
-    print("the year " + book + " was published was {} ".format(bookYear(int(a))))
+    print("the year " + book + " was published was {} ".format(bookdict.get(int(a))))
   elif (choice == "Genre") or (choice == "genre"):
-    print("the genre of " + book +  " is {}".format(bookGenre(int(a))))
+    print("the genre of " + book +  " is {}".format(bookdict.get(int(a))))
   elif (choice == "Pages") or (choice == "pages"):
-    print(book  + "has {} pages".format(bookPages(int(a))))
+    print(book  + "has {} pages".format(bookdict.get(int(a))))
   else:
     print("you didn't type anything correctly")
     book_info()
+
+
+bookdict = {
+  "title": "Hunger Games", "Little Women"
+  "author": "Suzanne Collins", "louisa May Alcott"
+  "year": 2008, 1868,
+  "genre": "dystopian", "coming-of-age"
+  "pages": 600, 670
+}
 
 bookTitles = ["Hunger Games", "Little Women"] 
 bookAuthors = ["Suzanne Collins", "Lousia May Alcott"]
