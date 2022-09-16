4ca9111ae33c08cce2c0fff1fc58576b88e5dd5f
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2022-09-16 03:20:25 +0000

    testing and comments. a good tidy of the code

--- main.py
+++ main.py
@@ -1,52 +1,5 @@
-def main ():
-  quit = True 
-  while quit == True:
-    menu = input('a) Add a book to the library database \n b) Find information about a chosen book \n c) change or add information about a book \n d) Quit \n')
-    if menu == 'a':
-      book_list()
-    elif menu == 'b':
-      book_info()
-    elif menu == 'c':
-      book_update()
-    elif menu == 'd':
-      quit == False
-    else:
-      main()
-
-    
-def book_list ():
- 
- title = input("Enter books title: ")
- dictionary.update({"title" : str(title)})
- author = input("Enter books author: ")
- dictionary.update({"author" : str(author)})
- year = input("Enter books year of publishing: ")
- dictionary.update({"year" : str(year)})
- genre = input("what genre is {}: ".format(title))
- dictionary.update({"genre" : str(genre)})
- pages = input("how many pages does {} have?: ".format(title))
- dictionary.update({"pages" : str(pages)})
- print(dictionary)
-
-
-def book_info ():
-  a = dictionary['title'].get(str(book))
-  print(a)
-  # a = int(a)
-  # choice = input("what do you want to know about {}. Please type: Author, Year,  Genre  or Pages: ".format(str(book)))
-  # if choice == "Author" or choice == "author":
-  #   print("the author of" + str(book) + "is {}".format(dictionary.get(a)))
-  # elif choice == "Year" or choice == "year":
-  #   print("the year " + str(book) + " was published was {}".format(dictionary['year'].get(a)))
-  # elif choice == "Genre" or choice == "genre":
-  #   print("the genre of " + str(book) + " is {}".format(dictionary['genre'].get(a)))
-  # elif choice == "Pages" or choice == "pages":
-  #   print(str(book) + "has {} pages".format(dictionary['pages'].get(a)))
-  # else:
-  #   print("you didn't type anything correctly")
-  #   book_info()
-
-def dict_zip ():
+# function for adding a new book (title, author, pages, genere) - press A
+def book_add():
   keys = ["author", "year", "genre", "pages"]
   title = input("Enter books title: ")
   author = input ("Enter books author: ")
@@ -57,31 +10,77 @@ def dict_zip ():
   data = dict(zip(keys,values))
   finaldata  = {title : data}
   print(finaldata)
-  dictionary.update(finaldata)
-  print(dictionary[str(title)]['author''year''genre''pages'])
+  books.update(finaldata)
+  
 
-def book_update():
+
+# function for printing out all books information  - press b 
+
+def book_info():
+ print(books)
+
+
+# function for modifying number of pages of a book  
+def book_pages():
   bookChoice = input("what book do you want to add info too?")
   choice1 = input("what section for {} do you want to add information too?".format(bookChoice))
   if (choice1 == "Author") or (choice1 == "author"):
     entered = input("enter the author of {}".format(bookChoice))
-    dictionary.update(entered)
+    books.update(entered)
   elif (choice1 == "Year") or (choice1 == "year"):
     entered = input("enter the year {} was published".format(bookChoice))
-    dictionary.update(entered)
+    books.update(entered)
   elif (choice1 == "Genre") or (choice1 == "genre"):
     entered = input("enter the genre of {}".format(bookChoice))
-    dictionary.update(entered)
+    books.update(entered)
   elif (choice1 == "Pages") or (choice1 == "pages"):
     entered = input("enter the number of pages in {}".format(bookChoice))
-    dictionary.update(entered)
+    books.update(entered)
+  else:
+    print("you didn't type anything correctly")
+    book_info()
+    
+  print('this is the updated version of the pages in your book')
+  book_page_print()
+
+
+# function for printing out the updated number of pages of a book - Press c
+
+def book_page_print ():
+  a = dictionary['title'].get(str(book))
+  print(a)
+  a = int(a)
+  choice = input("what do you want to know about {}. Please type: Author, Year,  Genre  or Pages: ".format(str(book)))
+  if choice == "Author" or choice == "author":
+    print("the author of" + str(book) + "is {}".format(dictionary.get(a)))
+  elif choice == "Year" or choice == "year":
+    print("the year " + str(book) + " was published was {}".format(dictionary['year'].get(a)))
+  elif choice == "Genre" or choice == "genre":
+    print("the genre of " + str(book) + " is {}".format(dictionary['genre'].get(a)))
+  elif choice == "Pages" or choice == "pages":
+    print(str(book) + "has {} pages".format(dictionary['pages'].get(a)))
   else:
     print("you didn't type anything correctly")
     book_info()
 
 
-dict_zip ()
-dictionary = {
+# main function - contains the menu 
+def main ():
+  quit = True 
+  print('what do you want to do?')
+  while quit == True:
+    menu = input('a) Add a book to the library database \n b) Print all book information \n c) adjust the pages in you chosen book \n d) Quit \n')
+    if menu == 'a':
+      book_add()
+    elif menu == 'b':
+      book_info()
+    elif menu =='c':
+      book_pages()
+    elif menu == 'd':
+      quit == False
+
+# the dictionary contains all the information entered in the program about the books 
+books = {
   'Hunger Games':{
     'author': 'Suzanne Collins',
     'year': '2008',
@@ -89,9 +88,38 @@ dictionary = {
     'pages': '600',
   }
 }
+
+# run main function, which is running the menu 
+main()
+
+# this prints the author of the hunger games which is stored in the dictionary (books). 
 print(dictionary['Hunger Games']['author'])
 
-keys = ["title", "author", "year", "genre", "pages"]
+
+
+# def book_list ():
+ 
+#  title = input("Enter books title: ")
+#  dictionary.update({"title" : str(title)})
+#  author = input("Enter books author: ")
+#  dictionary.update({"author" : str(author)})
+#  year = input("Enter books year of publishing: ")
+#  dictionary.update({"year" : str(year)})
+#  genre = input("what genre is {}: ".format(title))
+#  dictionary.update({"genre" : str(genre)})
+#  pages = input("how many pages does {} have?: ".format(title))
+#  dictionary.update({"pages" : str(pages)})
+#  print(dictionary)
+
+
+
+
+
+
+
+# keys = ["title", "author", "year", "genre", "pages"]
+
+
 # bookTitles = ["Hunger Games", "Little Women"] 
 # bookAuthors = ["Suzanne Collins", "Lousia May Alcott"]
 # bookYear = ["2008","1868"]
