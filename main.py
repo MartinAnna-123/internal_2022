cb1682a9db7882f67c8030d196eb4ab6bf9ec6b6
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2022-09-30 02:00:47 +0000

    i was adding in additional comments and came across a problem in function 'd'.  the problems was that if a user entered a title that wasn't in the function the program would have an attribute error and stop.  to fix this i added a try and except. i also created a new variable called bookEdited which changes the first letter of every word of the title entered by the user to a capital so that it is consistent with the dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# function for adding a new book (title, author, pages, genere) - press A
+# function for adding a new book (title, author, pages, genre) - press A
 def book_add():
   keys = ["author", "year", "genre", "pages"]
   title = input("Enter books title: ")
@@ -23,7 +23,9 @@ def book_info():
 
 
 # function for modifying number of pages of a book  - press C
+
 def book_pages():
+  # a while loop that will continue until the user enters a title of a book that is in the dictionary: books
   valid = False
   while valid == False:
     bookChoice = input("what book do you want to adjust the pages of?")
@@ -34,8 +36,8 @@ def book_pages():
     except AttributeError:
         print("Oops, the book you have entered is not in the system - try again")
   
-  print('The '+ bookChoiceEdited + ' currently has ' + bookChoicePages + ' pages')
-  
+  print(bookChoiceEdited + ' currently has ' + bookChoicePages + ' pages')
+  # once the user has entered a title that is in the dictionary and automatically exited the previous loop they will enter this second loop which requires them to enter a new number of pages. This loop will continue looping if an integer is not added.
   valid = False
   while valid == False:
     newPages = input("what do you want to change the pages to? please enter a number")
@@ -44,18 +46,17 @@ def book_pages():
       valid = True
     except ValueError:
         print("Oops, that was not an integer - try again")
-        
+  # the new page number is replaces the old page number in the dictionary
   books[bookChoiceEdited]['pages'] = newPages
 
-# if statement adjusting the output depending on the number the user enters
+  # if statement adjusting the output depending on the number the user enters
   if int(newPages) > 1:
     print(bookChoiceEdited, "now has", newPages, "Pages")
   elif int(newPages) == 1:
     print(bookChoiceEdited, "now has", newPages, "Page")
   else:
     print(bookChoiceEdited, "now has", newPages, "Pages")
-  # else:
-  #   print(bookChoice, " does not exist.")
+  
 
   
   
@@ -65,23 +66,27 @@ def book_pages():
 
 def book_page_print ():
   book = input("please type a book title that you want to know the number of pages\n")
-  page_print = True 
-  while page_print == True:
-    if str(book) in books:
-      a = books[str(book)]['pages']
+  bookEdited = book.title()
+  valid = False 
+  while valid == False:
+    try:
+      a = books[str(bookEdited)]['pages']
       # if statement adjusting the output depending on the number the user enters
       if int(a) > 1:
-        print('There are ' + a + ' pages in {}'.format(book))
+        print('There are ' + a + ' pages in {}'.format(bookEdited))
+        valid = True
       elif int(a) == 1:
-        print('There is ' + a + ' page in {}'.format(book))
-      page_print = False
-    else:
-      print('INVALID. Please follow the instructions')
-      book_page_print ()
+        print('There is ' + a + ' page in {}'.format(bookEdited))
+        valid = True
+      else:
+        print('INVALID. Please follow the instructions')
+        book_page_print ()
+    except AttrubuteError:
+      print("Oops, the book you have entered is not in the system - try again")
 
-# the dictionary contains all the information entered in the program about the books 
+# the dictionary contains all the information entered in the program about the books. The hunger games is in 'books' to format the dictionary.
 books = {
-  'Hunger Games':{
+  'The Hunger Games':{
     'author': 'Suzanne Collins',
     'year': '2008',
     'genre': 'Dystopian',
@@ -90,24 +95,31 @@ books = {
   
 }
 
-# main function - contains the menu 
-
+# the introduction. The initial text the user sees when they press run
 print('what do you want to do?')
 quit = True
+# The main menu is in a while loop so it will repeat until the user selects 'e' which is quit. The main menu navigates to all the functions and 'controls' the program
 while quit == True:
   menu = input('\n a) Add a book to the library database \n b) Print all book information \n c) Modify the pages in your chosen book \n d) print the amount of pages in your chosen book \n e) Quit \n')
+  # depending on what the users input is, the if statement will direct them to the corresponding function
   if menu == 'a':
     book_add()
+    print('what do you want to do next?')
   elif menu == 'b':
     book_info()
+    print('what do you want to do next?')
   elif menu =='c':
     book_pages()
+    print('what do you want to do next?')
   elif menu == 'd':
     book_page_print()
+    print('what do you want to do next?')
   elif menu == 'e':
     print('You have quit the program.')
+    # this will break the loop and the program will end
     quit = False
   else:
+    # if the user hasn't entered a,b,c,d or e the loop will repeat and the user will be required to enter a letter again
     print('please enter either: a, b, c, d or e')
   
     
